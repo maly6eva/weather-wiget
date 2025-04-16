62e39511aa996708aa0c34729c51f3f5c1e8e78f
Ksenia Malysheva <maly6eva.ksenia@gmail.com>
2025-04-16 13:42:02 +0300

    refactor

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,28 @@ import "./index.css";
 
 
 
+
 const KEY = 'fe355d4575454cef9f993402251404'
 
 function App() {
-  const [city, setCity] = useState('Timkovichi')
+  const [city, setCity] = useState('')
   const [weatherData, setWeatherData] =  useState(null)
   const [error, setError] = useState(null)
+  const [loading, setloading] = useState(false)
 
   useEffect(() => {
+    if(!city.trim()) return
     async function getData() {
+
+      setloading(true)
+      setError(null)
       try {
         const res = await fetch(
             `http://api.weatherapi.com/v1/current.json?key=${KEY}&q=${city}`
         )
 
         const data = await res.json();
-        console.log(data)
+
 
         if(data.error){
           setError(data.error.message)
@@ -28,16 +34,38 @@ function App() {
       } catch (error) {
         console.log(error)
         setError(error.message)
+      }finally{
+        setloading(false)
       }
     }
     getData()
-  }, [])
-
-  console.log(weatherData)
+  }, [city])
 
 
 
 
+function renderError(){
+    return <p>{error}</p>
+}
+
+  function renderLoading(){
+    return <p>Loading...</p>
+  }
+
+  function renderWeather(){
+    return (
+        <div className="weather-card">
+          <h2>{`${weatherData?.location?.name}, ${weatherData?.location?.country}`}</h2>
+          <img src={`https:${weatherData?.current?.condition?.icon}`} alt="icon" className="weather-icon" />
+          <p className="temperature">{`${Math.round(  weatherData?.current.temp_c)} °C`}</p>
+          <p className="condition">{weatherData?.current?.condition?.text}</p>
+          <div className="weather-details" >
+            <p>Влажность: {weatherData?.current?.humidity}%</p>
+            <p>Ветер: {weatherData?.current?.wind_kph}km/h</p>
+          </div>
+        </div>
+    )
+  }
 
 
   return (
@@ -46,21 +74,19 @@ function App() {
         <div className="weather-card-container">
           <h1 className="app-title">Weather Widget</h1>
           <div className="search-container">
-            <input type="text" placeholder="Enter city name" className="search-input" />
+            <input type="text"
+                   value={city}
+                   placeholder="Enter city name"
+                   className="search-input"
+                   onChange={(e) => setCity(e.target.value) }
+
+            />
           </div>
         </div>
+        {error && renderError()}
+        {loading && renderLoading()}
+        {!loading && !error && weatherData && renderWeather()}
 
-            <div className="weather-card">
-              <h2>{`${weatherData?.location?.name}, ${weatherData?.location?.country}`}</h2>
-              <img src={`https:${weatherData?.current?.condition?.icon}`} alt="icon" className="weather-icon" />
-              <p className="temperature">{Math.round(  weatherData?.current.temp_c)
-              }</p>
-              <p className="condition">{weatherData?.current?.condition?.text}</p>
-              <div className="weather-details" >
-                <p>Humidity: {weatherData?.current?.humidity}%</p>
-                <p>Wind: {weatherData?.current?.wind_kph}km/h</p>
-              </div>
-            </div>
       </div>
     </div>
   );
